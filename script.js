62056dacc6d764bd96d7330f2001f8377cfd5514
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-26 13:21:08 +0100

    fixed script popup working

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-// JavaScript
 document.addEventListener('DOMContentLoaded', function () {
   const shareIcon = document.querySelector('.share-icon');
   const popup = document.getElementById('popup');
+
+  let isOpen = false;
 
   console.log(shareIcon);
   console.log(popup);
 
   shareIcon.addEventListener('click', function () {
+    isOpen = !isOpen;
     console.log('Clicked');
-    popup.classList.toggle('show');
+    if (isOpen) {
+      popup.style.display = 'block';
+    } else {
+      popup.style.display = 'none';
+    }
   });
 });
