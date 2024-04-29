74a406f2a112c09e1bf91443bd21558f131c5ac9
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-04-29 16:31:40 +0100

    trying to fix position of popup container

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   let isOpen = false;
 
-  console.log(shareIcon);
-  console.log(popup);
-
   shareIcon.addEventListener('click', function () {
     isOpen = !isOpen;
-    console.log('Clicked');
+
     if (isOpen) {
       popup.style.display = 'block';
     } else {
